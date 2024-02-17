6aa303b635d8146f44d4741a7bdf1eaa4674d809
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-17 20:40:28 -0300

    añadimos las rutas a la pagina 404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,11 +1,13 @@
 import { Button } from 'react-bootstrap';
 import error from '../../assets/error404.png'
+import {NavLink } from 'react-router-dom';
+
 const Error404 = () => {
     return (
         <section className="mainSection text-center">
             <img src={error} alt="error 404" />
             <div>
-                <Button variant='success' className='mb-5'>Volver al inicio</Button>
+                <NavLink className='mb-5 btn btn-success' to="/">Volver al inicio</NavLink>
             </div>
         </section>
     );
